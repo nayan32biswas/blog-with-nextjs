5ce47ab55af5a957d806f6c2e48829655254a615
Nayan Biswas <nayan32biswas@gmail.com>
2023-05-18 11:05:06 -0600

    update

--- src/components/navbar/SearchModal.tsx
+++ src/components/navbar/SearchModal.tsx
@@ -1,102 +1,95 @@
-import { useState } from "react";
+import * as React from "react";
 
-import SearchIcon from "@mui/icons-material/Search";
-import Backdrop from "@mui/material/Backdrop";
-import Box from "@mui/material/Box";
-import Fade from "@mui/material/Fade";
-import IconButton from "@mui/material/IconButton";
-import InputBase from "@mui/material/InputBase";
-import Modal from "@mui/material/Modal";
+import {
+  Avatar,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
 import Typography from "@mui/material/Typography";
-import { alpha, styled } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2)
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(2)
+  }
+}));
+
+const styles = {
+  textInput: {
+    fontSize: "large"
+  }
+};
+
+export interface DialogTitleProps {
+  id: string;
+  children?: React.ReactNode;
+  onClose: () => void;
+}
 
 export default function SearchModal() {
-  const [openSearch, setOpenSearch] = useState(false);
-  console.log("search box", openSearch);
+  const [open, setOpen] = React.useState(true);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  // const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [0, 1, 2, 3, 4, 5, 6];
+
   return (
     <div>
-      {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      <IconButton component="label" onClick={() => setOpenSearch(true)}>
-        <SearchIcon />
-      </IconButton>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={openSearch}
-        onClose={() => setOpenSearch(false)}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500
-          }
-        }}
-      >
-        <Fade in={openSearch}>
-          <Box sx={style}>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
-            </Search>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, velit sunt! Nemo
-              velit accusantium ullam atque minima? Impedit quae, quibusdam veniam voluptas nostrum
-              laudantium reiciendis velit laborum possimus neque saepe.
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Search
+      </Button>
+      <BootstrapDialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <TextField
+          type="search"
+          id="search"
+          placeholder="Search..."
+          inputProps={styles.textInput}
+        />
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          {numbers.map((val: number, idx: number) => {
+            return (
+              <>
+                <ListItem alignItems="flex-start" key={idx}>
+                  <ListItemAvatar>
+                    <Avatar alt="Profile Avatar" />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Post Title"
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque fuga
+                          tenetur odio aspernatur mollitia, vitae voluptatibus maiores.
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                {idx + 1 !== numbers.length ? <Divider variant="inset" component="li" /> : null}
+              </>
+            );
+          })}
+        </List>
+      </BootstrapDialog>
     </div>
   );
 }
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "30%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 1
-};
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center"
-}));
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25)
-  },
-  //   marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    // marginLeft: theme.spacing(3),
-    width: "auto"
-  }
-}));
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch"
-    }
-  }
-}));
