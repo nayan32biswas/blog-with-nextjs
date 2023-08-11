7f3a00009a630f66eb1980c20faaa815f04d69ac
Nayan Biswas <nayan32biswas@gmail.com>
2023-08-11 17:29:40 +0600

    resole some issue for comment

---
+++ src/components/comments/CommentReplies.tsx
@@ -0,0 +1,86 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import CardHeader from '@mui/material/CardHeader';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+
+import { IReply } from '@/types/api.types';
+import { toLocaleDateString } from '@/utils';
+
+interface CommentRepliesProps {
+  replies: IReply[];
+  reply_box_open?: boolean;
+}
+
+function CommentReplies({ replies, reply_box_open }: CommentRepliesProps) {
+  if (!replies) {
+    return (
+      <Typography component="h1" variant="h4">
+        Loading...
+      </Typography>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        {reply_box_open === true ? <TextField /> : null}
+        {replies.map((reply: IReply, idx: number) => {
+          return (
+            <Container key={`reply-${reply.id}`} maxWidth="sm">
+              {idx !== 0 ? <Typography component={'hr'} /> : null}
+              <CardHeader
+                avatar={
+                  <Link href={`/@${reply?.user?.username}`}>
+                    {reply.user ? (
+                      <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        {reply.user.image ? (
+                          <Image
+                            height={30}
+                            width={30}
+                            src={reply.user.image}
+                            alt="Author Avatar"
+                          />
+                        ) : (
+                          reply.user.full_name[0]
+                        )}
+                      </Avatar>
+                    ) : null}
+                  </Link>
+                }
+                title={<Link href={`/@${reply?.user?.username}`}>{reply?.user?.full_name}</Link>}
+                subheader={
+                  <Typography>
+                    <Typography component={'span'} sx={{ color: 'gray' }}>
+                      Published At:
+                    </Typography>{' '}
+                    <Typography component={'span'}>
+                      {toLocaleDateString(reply.created_at)}
+                    </Typography>
+                  </Typography>
+                }
+              />
+              <Container maxWidth="md">
+                <Typography component="p">{reply.description}</Typography>
+              </Container>
+            </Container>
+          );
+        })}
+      </Box>
+    </React.Fragment>
+  );
+}
+
+export default CommentReplies;
--- src/components/comments/PostComments.tsx
+++ src/components/comments/PostComments.tsx
@@ -3,50 +3,70 @@ import Link from 'next/link';
 import React from 'react';
 
 import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
 import CardHeader from '@mui/material/CardHeader';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 
 import { handleAxiosError } from '@/api/apiUtils/AxiosConfig';
 import { fetchComments } from '@/api/postApi';
-import { IComment, ICommentList, IReply } from '@/types/api.types';
+import { IComment, ICommentList } from '@/types/api.types';
 import { getListApiDefaultValue, toLocaleDateString } from '@/utils';
+
+import CommentReplies from './CommentReplies';
 
 interface PostCommentsProps {
   post_slug: string;
 }
 
 function PostComments({ post_slug }: PostCommentsProps) {
-  const limit = 20;
+  const limit = 5;
 
   const [currentPage, setCurrentPage] = React.useState<number>(1);
-  const [commentData, setCommentData] = React.useState<ICommentList | null>(null);
+  const [comments, setComments] = React.useState<IComment[]>([]);
+  const [totalComment, setTotalComment] = React.useState<number>(0);
 
   React.useEffect(() => {
-    const fetchData = async () => {
+    function margeComments(newComments: IComment[]) {
+      const commentIds: Set<string> = new Set();
+      const commentList = [...comments];
+      commentList.forEach((comment: IComment) => {
+        commentIds.add(comment.id);
+      });
+
+      newComments.forEach((comment: IComment) => {
+        if (commentIds.has(comment.id) === false) {
+          commentList.push(comment);
+        }
+      });
+      setComments(commentList);
+    }
+
+    const fetchData = async (post_slug: string, page: number) => {
       let commentData: ICommentList = getListApiDefaultValue();
       try {
         commentData = await fetchComments({
           post_slug,
-          params: { page: currentPage, limit: limit }
+          params: { page, limit: limit }
         });
       } catch (e: any) {
         const { message: errorMessage } = handleAxiosError(e);
         commentData.errorMessage = errorMessage;
       }
-      setCommentData(commentData);
-    };
 
-    fetchData().catch((e) => {
+      setTotalComment(commentData.count);
+      margeComments(commentData.results);
+    };
+    fetchData(post_slug, currentPage).catch((e) => {
       // handle the error as needed
       console.error('An error occurred while fetching the data: ', e);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [post_slug, currentPage]);
 
-  if (!commentData) {
+  if (!comments) {
     return (
       <Typography component="h1" variant="h4">
         Loading...
@@ -57,10 +77,10 @@ function PostComments({ post_slug }: PostCommentsProps) {
   return (
     <React.Fragment>
       <TextField />
-      {commentData.results.map((comment: IComment) => {
+      {comments.map((comment: IComment, idx: number) => {
         return (
           <Container key={comment.id} maxWidth="sm">
-            <Typography component={'hr'} />
+            {idx !== 0 ? <Typography component={'hr'} /> : null}
             <CardHeader
               avatar={
                 <Link href={`/@${comment?.user?.username}`}>
@@ -93,62 +113,23 @@ function PostComments({ post_slug }: PostCommentsProps) {
               }
             />
             <Container maxWidth="md">
-              <Typography component="p">{comment.description}</Typography>
+              <Typography component="div">
+                <Typography component="p">{comment.description}</Typography>
+              </Typography>
             </Container>
-            <Box
-              sx={{
-                my: 4,
-                display: 'flex',
-                flexDirection: 'column'
-              }}
-            >
-              {comment.replies.map((reply: IReply) => {
-                return (
-                  <Container key={`reply-${reply.id}`} maxWidth="sm">
-                    <Typography component={'hr'} />
-                    <CardHeader
-                      avatar={
-                        <Link href={`/@${reply?.user?.username}`}>
-                          {reply.user ? (
-                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                              {reply.user.image ? (
-                                <Image
-                                  height={30}
-                                  width={30}
-                                  src={reply.user.image}
-                                  alt="Author Avatar"
-                                />
-                              ) : (
-                                reply.user.full_name[0]
-                              )}
-                            </Avatar>
-                          ) : null}
-                        </Link>
-                      }
-                      title={
-                        <Link href={`/@${reply?.user?.username}`}>{reply?.user?.full_name}</Link>
-                      }
-                      subheader={
-                        <Typography>
-                          <Typography component={'span'} sx={{ color: 'gray' }}>
-                            Published At:
-                          </Typography>{' '}
-                          <Typography component={'span'}>
-                            {toLocaleDateString(reply.created_at)}
-                          </Typography>
-                        </Typography>
-                      }
-                    />
-                    <Container maxWidth="md">
-                      <Typography component="p">{reply.description}</Typography>
-                    </Container>
-                  </Container>
-                );
-              })}
-            </Box>
+            <CommentReplies replies={comment.replies} reply_box_open={false} />
           </Container>
         );
       })}
+      {totalComment > currentPage * limit ? (
+        <Typography component="div">
+          <Grid container justifyContent="center">
+            <Typography component={'button'} onClick={() => setCurrentPage(currentPage + 1)}>
+              Load More
+            </Typography>
+          </Grid>
+        </Typography>
+      ) : null}
     </React.Fragment>
   );
 }
