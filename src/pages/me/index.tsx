fb3d4a26e3d66a7f0cecc3358c9d511a8eda7cea
Nayan Biswas <nayan32biswas@gmail.com>
2023-08-13 22:45:30 +0600

    post done

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,62 +1,11 @@
-import Link from 'next/link';
 import React from 'react';
-
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Theme, useTheme } from '@mui/material/styles';
-
-const getStyles = (theme: Theme) => {
-  const mode = theme.palette.mode;
-  return {
-    menuBorder: {
-      position: 'relative',
-      outline: '0px',
-      margin: 'auto',
-      marginTop: '5px',
-      display: 'flex',
-      justifyContent: 'center',
-      padding: '24px',
-      backgroundColor: mode == 'light' ? 'rgb(240, 240, 240)' : 'rgb(25, 25, 30)'
-    },
-    tabButton: {}
-  };
-};
 
 export const config = {
   matcher: ['/me']
 };
 
 function Me() {
-  const theme = useTheme<Theme>();
-  const styles = getStyles(theme);
-
-  const tabKeys = {
-    personalInfo: 'personal-info',
-    security: 'security'
-  };
-
-  return (
-    <Typography component="div">
-      <Typography component="div" sx={styles.menuBorder}>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            whiteSpace: '20px'
-          }}
-        >
-          <Link href={`/me#${tabKeys.personalInfo}`}>
-            <Button>Personal Info</Button>
-          </Link>
-          <Link href={`/me#${tabKeys.security}`}>
-            <Button>Security</Button>
-          </Link>
-        </Box>
-      </Typography>
-    </Typography>
-  );
+  return <div>Me</div>;
 }
 
 export default Me;
